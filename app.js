67a0b4467669be6022e4675a7590695ecda5a48d
Dakengoi <dako010203@mail.ru>
2024-03-01 05:43:02 +0600

    render

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ const app = express();
 const port = 3000;
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL ,
-})
-pool.connect((err) => {
-  if (err) throw err
-  console.log("Connect to PostgreSQL successfully!")
-})
+  user: "postgres",
+  host: "127.0.0.1",
+  database: "social-media",
+  password: "asdfghjkl;'",
+  port: 5432,
+});
+
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
